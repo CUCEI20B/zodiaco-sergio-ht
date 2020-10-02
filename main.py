7b4c610bec2a6180f1c2bc4390ac7186a689e5e8
Sergio Hinojosa <sergio.hinojosa2945@alumnos.udg.mx>
2020-10-02 16:42:57 -0500

    Prueba 01

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
-print("Hola Mundo")
+dia = int(input(dia: ))
+mes = input(mes: )
+
+if(mes == "enero"):
+    if(dia < 20):
+        print("capricornio")
+    else:
+        print("acuario")
+elif(mes == "febrero"):
+    if(dia < 20):
+        print("acuario")
+    else:
+        print("piscis")
+elif(mes == "marzo"):
+    if(dia < 21):
+        print("piscis")
+    else:
+        print("aries")
+elif(mes == "abril"):
+    if(dia < 21):
+        print("aries")
+    else:
+        print("tauro")
+elif(mes == "mayo"):
+    if(dia < 21):
+        print("tauro")
+    else:
+        print("gemini")
+elif(mes == "junio"):
+    if(dia < 21):
+        print("gemini")
+    else:
+        print("cancer")
+elif(mes == "julio"):
+    if(dia < 23):
+        print("cancer")
+    else:
+        print("leo")
+elif(mes == "agosto"):
+    if(dia < 23):
+        print("leo")
+    else:
+        print("virgo")
+elif(mes == "septiembre"):
+    if(dia < 23):
+        print("virgo")
+    else:
+        print("libra")
+elif(mes == "octubre"):
+    if(dia < 23):
+        print("libra")
+    else:
+        print("scorpio")
+elif(mes == "noviembre"):
+    if(dia < 23):
+        print("scorpio")
+    else:
+        print("sagitario")
+elif(mes == "diciembre"):
+    if(dia < 22):
+        print("sagitario")
+    else:
+        print("capricornio")
